10cd84482679a34542ff1d2daf603c6b9e205fc1
Henrique Brito Elias <henriquebelias@gmail.com>
2021-10-31 13:40:44 -0300

    multipleCansPossibility refactor

--- src/utils/calcCanOptions.js
+++ src/utils/calcCanOptions.js
@@ -9,21 +9,23 @@ export const cansPossibilities = (liters, cans) => {
 }
 
 export const multipleCansPossibility = (liters, cans) => {
+  const currentPosition = cans[cans.length - 1];
+
   if (liters < 0) {
     return cans;
   }
 
-  if (liters - cans[cans.length -1]['liters'] < 0 && cans.length === 1) {
-    cans[cans.length -1]['counter'] += 1;
+  if (liters - currentPosition['liters'] < 0 && cans.length === 1) {
+    currentPosition['counter'] += 1;
 
     return cans;
-  } else if (cans[cans.length -1]['liters'] > liters) {
+  } else if (currentPosition['liters'] > liters) {
     return multipleCansPossibility(liters, cans.slice(0, -1));
-  } else if (liters - cans[cans.length -1]['liters'] < 0) {
+  } else if (liters - currentPosition['liters'] < 0) {
     return multipleCansPossibility(liters, cans.slice(0, -1));
   } else {
-    liters -= cans[cans.length -1]['liters'];
-    cans[cans.length -1]['counter'] += 1;
+    liters -= currentPosition['liters'];
+    currentPosition['counter'] += 1;
 
     return cans.concat(multipleCansPossibility(liters, cans));
   }
